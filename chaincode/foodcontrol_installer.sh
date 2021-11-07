9cf3471aecf6562dac563f53500e9750f5053ae5
Mauro Emmanuel Rambo <mauroemmanuel22@gmail.com>
2021-11-07 13:45:50 -0300

    fix commit cmd

--- chaincode/foodcontrol_installer.sh
+++ chaincode/foodcontrol_installer.sh
@@ -11,6 +11,9 @@ export CHAINCODE_NAME=foodcontrol
 export CHAINCODE_VERSION=1
 export CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/$CHAINCODE_NAME/"
 export ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/acme.com/orderers/orderer.acme.com/msp/tlscacerts/tlsca.acme.com-cert.pem
+export CORE_PEER_PATH_ORG1=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.acme.com/
+export CORE_PEER_PATH_ORG2=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.acme.com/
+export CORE_PEER_PATH_ORG3=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.acme.com/
 
 #Empaquetar los smart contracts
 peer lifecycle chaincode package ${CHAINCODE_NAME}.tar.gz \
@@ -21,30 +24,30 @@ peer lifecycle chaincode package ${CHAINCODE_NAME}.tar.gz \
 #Instalar smart contracts en los peers
 peer lifecycle chaincode install ${CHAINCODE_NAME}.tar.gz
 
-CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.acme.com/users/Admin@org2.acme.com/msp \
+CORE_PEER_MSPCONFIGPATH=$CORE_PEER_PATH_ORG2/users/Admin@org2.acme.com/msp \
     CORE_PEER_ADDRESS=peer0.org2.acme.com:7051 \
     CORE_PEER_LOCALMSPID="Org2MSP" \
-    CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.acme.com/peers/peer0.org2.acme.com/tls/ca.crt \
+    CORE_PEER_TLS_ROOTCERT_FILE=CORE_PEER_PATH_ORG2/peers/peer0.org2.acme.com/tls/ca.crt \
     peer lifecycle chaincode install ${CHAINCODE_NAME}.tar.gz
 
-CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.acme.com/users/Admin@org3.acme.com/msp \
+CORE_PEER_MSPCONFIGPATH=$CORE_PEER_PATH_ORG3/users/Admin@org3.acme.com/msp \
     CORE_PEER_ADDRESS=peer0.org3.acme.com:7051 \
     CORE_PEER_LOCALMSPID="Org3MSP" \
-    CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.acme.com/peers/peer0.org3.acme.com/tls/ca.crt \
+    CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_PATH_ORG3/peers/peer0.org3.acme.com/tls/ca.crt \
     peer lifecycle chaincode install ${CHAINCODE_NAME}.tar.gz
 
-#Definir políticas de endorsamiento (primera y segunda org pueden recibir transacciones)
+#Definir políticas de endorsamiento (primera y tercera org pueden escribir)
 peer lifecycle chaincode approveformyorg \
     --channelID $CHANNEL_NAME \
     --name ${CHAINCODE_NAME} \
     --version ${CHAINCODE_VERSION} \
-    --sequence 1 --tls true --cafile $ORDERER_CA --waitForEvent \
+    --sequence 1 --tls --cafile $ORDERER_CA --waitForEvent \
     --signature-policy "OR ('Org1MSP.peer','Org3MSP.peer')" --package-id $CC_PACKAGE_ID
 
-CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.acme.com/users/Admin@org3.acme.com/msp \
+CORE_PEER_MSPCONFIGPATH=$CORE_PEER_PATH_ORG3/users/Admin@org3.acme.com/msp \
     CORE_PEER_ADDRESS=peer0.org3.acme.com:7051 \
     CORE_PEER_LOCALMSPID="Org3MSP" \
-    CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.acme.com/peers/peer0.org3.acme.com/tls/ca.crt \
+    CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_PATH_ORG3/peers/peer0.org3.acme.com/tls/ca.crt \
     peer lifecycle chaincode approveformyorg \
     --channelID $CHANNEL_NAME \
     --name ${CHAINCODE_NAME} \
@@ -52,17 +55,33 @@ CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypt
     --sequence 1 --tls --cafile $ORDERER_CA --waitForEvent \
     --signature-policy "OR ('Org1MSP.peer','Org3MSP.peer')" --package-id $CC_PACKAGE_ID
 
-#Comitear los cambios
-peer lifecycle chaincode commit -o orderer.acme.com:7050 \
+
+#Verificar si se aplicaron correctamente las políticas de endorsamiento
+peer lifecycle chaincode checkcommitreadiness \
     --channelID $CHANNEL_NAME \
     --name ${CHAINCODE_NAME} \
     --version ${CHAINCODE_VERSION} \
-    --sequence 1 --tls --cafile $ORDERER_CA --waitForEvent \
-    --peerAddresses peer0.org1.acme.com:7051 \
-    --peerAddresses peer0.org2.acme.com:7051 \
-    --peerAddresses peer0.org3.acme.com:7051 \
-    --signature-policy "OR ('Org1MSP.peer','Org2MSP.peer','Org3MSP.peer')" \
-    --output json --init-required
+    --sequence 1 \
+    --signature-policy "OR ('Org1MSP.peer','Org3MSP.peer')" \
+    --output json
+    
 
-peer chaincode invoke -o orderer.acme.com:7050 --tls true --cafile $ORDERER_CA \
-    -C $CHANNEL_NAME -n ${CHAINCODE_NAME} -c '{"Args":["initLedger"]}'
+#Comitear los cambios
+peer lifecycle chaincode commit -o orderer.acme.com:7050 \
+    --tls --cafile $ORDERER_CA
+    --peerAddresses peer0.org1.acme.com:7051 \
+    --tlsRootCertFiles $CORE_PEER_PATH_ORG1/peers/peer0.org1.acme.com/tls/ca.crt \
+    --peerAddresses peer0.org3.acme.com:7051 \
+    --tlsRootCertFiles $CORE_PEER_PATH_ORG3/peers/peer0.org3.acme.com/tls/ca.crt \
+    --channelID $CHANNEL_NAME \
+    --name ${CHAINCODE_NAME} \
+    --version ${CHAINCODE_VERSION} \
+    --sequence 1 \
+    --signature-policy "OR ('Org1MSP.peer', 'Org3MSP.peer')"
+
+#Test
+peer chaincode invoke -o orderer.acme.com:7050 
+    --tls --cafile $ORDERER_CA \
+    -C $CHANNEL_NAME \
+    -n $CHAINCODE_NAME \
+    -c '{"Args":["Set","did:3","mauro","frutilla"]}'
